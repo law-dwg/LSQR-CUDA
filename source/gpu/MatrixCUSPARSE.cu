3338b5aab5719237451546fabbccc987f6cb8b50
law-dwg <lawayers@vt.edu>
2021-10-17 22:38:14 +0200

    PMPP-47

--- source/gpu/MatrixCUSPARSE.cu
+++ source/gpu/MatrixCUSPARSE.cu
@@ -5,10 +5,10 @@ Vector_GPU MatrixCUSPARSE::operator*(Vector_GPU &v) {
   cusparseDnVecDescr_t rhs_desc, out_desc;
   cusparseErrCheck(cusparseCreateDnVec(&rhs_desc, out.getRows(), v.d_mat, CUDA_R_64F));
   cusparseErrCheck(cusparseCreateDnVec(&out_desc, out.getRows(), out.d_mat, CUDA_R_64F));
-  cusparseErrCheck(cusparseSpMV_bufferSize(spHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &ONE, this->d_matDescr_sparse, rhs_desc, &ZERO, out_desc,
-                                           CUDA_R_64F, CUSPARSE_MV_ALG_DEFAULT, &bufferSize));
+  cusparseErrCheck(cusparseSpMV_bufferSize(spHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &ONE, this->spMatDescr, rhs_desc, &ZERO, out_desc, CUDA_R_64F,
+                                           CUSPARSE_MV_ALG_DEFAULT, &bufferSize));
   // cudaErrCheck(cudaMalloc(&dBuffer, this->bufferSize));
-  cusparseErrCheck(cusparseSpMV(spHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &ONE, this->d_matDescr_sparse, rhs_desc, &ZERO, out_desc, CUDA_R_64F,
+  cusparseErrCheck(cusparseSpMV(spHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &ONE, this->spMatDescr, rhs_desc, &ZERO, out_desc, CUDA_R_64F,
                                 CUSPARSE_MV_ALG_DEFAULT, dBuffer));
 
   return out;
@@ -18,34 +18,34 @@ Vector_CUBLAS MatrixCUSPARSE::operator*(Vector_CUBLAS &v) {
   cusparseDnVecDescr_t rhs_desc, out_desc;
   cusparseErrCheck(cusparseCreateDnVec(&rhs_desc, out.getRows(), v.d_mat, CUDA_R_64F));
   cusparseErrCheck(cusparseCreateDnVec(&out_desc, out.getRows(), out.d_mat, CUDA_R_64F));
-  cusparseErrCheck(cusparseSpMV_bufferSize(spHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &ONE, this->d_matDescr_sparse, rhs_desc, &ZERO, out_desc,
-                                           CUDA_R_64F, CUSPARSE_MV_ALG_DEFAULT, &bufferSize));
+  cusparseErrCheck(cusparseSpMV_bufferSize(spHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &ONE, this->spMatDescr, rhs_desc, &ZERO, out_desc, CUDA_R_64F,
+                                           CUSPARSE_MV_ALG_DEFAULT, &bufferSize));
   // cudaErrCheck(cudaMalloc(&dBuffer, this->bufferSize));
-  cusparseErrCheck(cusparseSpMV(spHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &ONE, this->d_matDescr_sparse, rhs_desc, &ZERO, out_desc, CUDA_R_64F,
+  cusparseErrCheck(cusparseSpMV(spHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &ONE, this->spMatDescr, rhs_desc, &ZERO, out_desc, CUDA_R_64F,
                                 CUSPARSE_MV_ALG_DEFAULT, dBuffer));
 
   return out;
 };
 MatrixCUSPARSE MatrixCUSPARSE::transpose() {
   MatrixCUSPARSE out(this->h_columns, this->h_rows, this->h_nnz);
-  cudaErrCheck(cudaMemset(out.d_csr_columns, ZERO, out.h_nnz * sizeof(int)));
-  cudaErrCheck(cudaMemset(out.d_csr_values, ZERO, out.h_nnz * sizeof(double)));
-  cusparseErrCheck(cusparseCsr2cscEx2_bufferSize(spHandle, this->h_rows, this->h_columns, this->h_nnz, this->d_csr_values, this->d_csr_offsets,
-                                                 this->d_csr_columns, out.d_csr_values, out.d_csr_offsets, out.d_csr_columns, CUDA_R_64F,
+  cudaErrCheck(cudaMemset(out.d_csrColInd, ZERO, out.h_nnz * sizeof(int)));
+  cudaErrCheck(cudaMemset(out.d_csrVal, ZERO, out.h_nnz * sizeof(double)));
+  cusparseErrCheck(cusparseCsr2cscEx2_bufferSize(spHandle, this->h_rows, this->h_columns, this->h_nnz, this->d_csrVal, this->d_csrRowPtr,
+                                                 this->d_csrColInd, out.d_csrVal, out.d_csrRowPtr, out.d_csrColInd, CUDA_R_64F,
                                                  CUSPARSE_ACTION_NUMERIC, CUSPARSE_INDEX_BASE_ZERO, CUSPARSE_CSR2CSC_ALG1, &out.bufferSize));
   cudaErrCheck(cudaMalloc(&out.dBuffer, out.bufferSize));
-  cusparseErrCheck(cusparseCsr2cscEx2(spHandle, this->h_rows, this->h_columns, this->h_nnz, this->d_csr_values, this->d_csr_offsets,
-                                      this->d_csr_columns, out.d_csr_values, out.d_csr_offsets, out.d_csr_columns, CUDA_R_64F,
-                                      CUSPARSE_ACTION_NUMERIC, CUSPARSE_INDEX_BASE_ZERO, CUSPARSE_CSR2CSC_ALG1, out.dBuffer));
+  cusparseErrCheck(cusparseCsr2cscEx2(spHandle, this->h_rows, this->h_columns, this->h_nnz, this->d_csrVal, this->d_csrRowPtr, this->d_csrColInd,
+                                      out.d_csrVal, out.d_csrRowPtr, out.d_csrColInd, CUDA_R_64F, CUSPARSE_ACTION_NUMERIC, CUSPARSE_INDEX_BASE_ZERO,
+                                      CUSPARSE_CSR2CSC_ALG1, out.dBuffer));
 
-  cusparseErrCheck(cusparseCreateCsr(&out.d_matDescr_sparse, out.h_rows, out.h_columns, out.h_nnz, out.d_csr_offsets, out.d_csr_columns,
-                                     out.d_csr_values, CUSPARSE_INDEX_32I, CUSPARSE_INDEX_32I, CUSPARSE_INDEX_BASE_ZERO, CUDA_R_64F));
+  cusparseErrCheck(cusparseCreateCsr(&out.spMatDescr, out.h_rows, out.h_columns, out.h_nnz, out.d_csrRowPtr, out.d_csrColInd, out.d_csrVal,
+                                     CUSPARSE_INDEX_32I, CUSPARSE_INDEX_32I, CUSPARSE_INDEX_BASE_ZERO, CUDA_R_64F));
   return out;
 };
 double MatrixCUSPARSE::Dnrm2() {
   double h_out;
   int incre = 1;
-  cublasErrCheck(cublasDnrm2(handle, (int)this->h_nnz, this->d_csr_values, incre, &h_out));
+  cublasErrCheck(cublasDnrm2(handle, (int)this->h_nnz, this->d_csrVal, incre, &h_out));
   assert(!(h_out != h_out));
   return h_out;
 };
--- source/gpu/MatrixCUSPARSE.cuh
+++ source/gpu/MatrixCUSPARSE.cuh
@@ -12,27 +12,27 @@ class MatrixCUSPARSE {
 protected:
 public:
   unsigned h_rows, h_columns, *d_rows, *d_columns;
-  int *d_csr_offsets, *d_csr_columns;
+  int *d_csrRowPtr, *d_csrColInd;
   int64_t h_nnz;
-  double *d_csr_values;
-  cusparseSpMatDescr_t d_matDescr_sparse = NULL;
+  double *d_csrVal;
+  cusparseSpMatDescr_t spMatDescr = NULL;
   cusparseMatDescr_t descr = NULL;
   void *dBuffer = NULL;
   size_t bufferSize = 0;
   MatrixCUSPARSE() : MatrixCUSPARSE(0, 0, 0) { // Default constructor
     printf("MatrixCUSPARSE Default Constructor called\n");
-    cudaErrCheck(cudaMemset(d_csr_values, ZERO, h_nnz * sizeof(double)));
-    cudaErrCheck(cudaMemset(d_csr_columns, ZERO, h_nnz * sizeof(int)));
-    cudaErrCheck(cudaMemset(d_csr_offsets, ZERO, (h_rows + 1) * sizeof(int)));
+    cudaErrCheck(cudaMemset(d_csrVal, ZERO, h_nnz * sizeof(double)));
+    cudaErrCheck(cudaMemset(d_csrColInd, ZERO, h_nnz * sizeof(int)));
+    cudaErrCheck(cudaMemset(d_csrRowPtr, ZERO, (h_rows + 1) * sizeof(int)));
   };
   MatrixCUSPARSE(unsigned r, unsigned c, int n) : h_rows(r), h_columns(c), h_nnz((int64_t)n) { // Helper constructor #1
     printf("MatrixCUSPARSE Helper Constructor #1 called\n");
     // memory allocation
     cudaErrCheck(cudaMalloc((void **)&d_rows, sizeof(unsigned)));
     cudaErrCheck(cudaMalloc((void **)&d_columns, sizeof(unsigned)));
-    cudaErrCheck(cudaMalloc((void **)&d_csr_values, h_nnz * sizeof(double)));
-    cudaErrCheck(cudaMalloc((void **)&d_csr_columns, h_nnz * sizeof(int)));
-    cudaErrCheck(cudaMalloc((void **)&d_csr_offsets, (h_rows + 1) * sizeof(int)));
+    cudaErrCheck(cudaMalloc((void **)&d_csrVal, h_nnz * sizeof(double)));
+    cudaErrCheck(cudaMalloc((void **)&d_csrColInd, h_nnz * sizeof(int)));
+    cudaErrCheck(cudaMalloc((void **)&d_csrRowPtr, (h_rows + 1) * sizeof(int)));
     cudaErrCheck(cudaMalloc(&dBuffer, bufferSize));
     // copy to device
     cudaErrCheck(cudaMemcpy(d_rows, &h_rows, sizeof(unsigned), cudaMemcpyHostToDevice));
@@ -42,21 +42,21 @@ public:
     cusparseErrCheck(cusparseSetMatType(descr, CUSPARSE_MATRIX_TYPE_GENERAL));
     cusparseErrCheck(cusparseSetMatIndexBase(descr, CUSPARSE_INDEX_BASE_ZERO));
   };
-  MatrixCUSPARSE(unsigned r, unsigned c, long long int n, double *values, int *columns, int *offsets)
+  MatrixCUSPARSE(unsigned r, unsigned c, long long int n, double *values, int *colInd, int *rowPtr)
       : MatrixCUSPARSE(r, c, n) { // Helper constructor #2
     printf("MatrixCUSPARSE Helper Constructor #2 called\n");
     // copy to device
-    cudaErrCheck(cudaMemcpy(d_csr_values, values, h_nnz * sizeof(double), cudaMemcpyDeviceToDevice));
-    cudaErrCheck(cudaMemcpy(d_csr_columns, columns, h_nnz * sizeof(int), cudaMemcpyDeviceToDevice));
-    cudaErrCheck(cudaMemcpy(d_csr_offsets, offsets, (h_rows + 1) * sizeof(int), cudaMemcpyDeviceToDevice));
+    cudaErrCheck(cudaMemcpy(d_csrVal, values, h_nnz * sizeof(double), cudaMemcpyDeviceToDevice));
+    cudaErrCheck(cudaMemcpy(d_csrColInd, colInd, h_nnz * sizeof(int), cudaMemcpyDeviceToDevice));
+    cudaErrCheck(cudaMemcpy(d_csrRowPtr, rowPtr, (h_rows + 1) * sizeof(int), cudaMemcpyDeviceToDevice));
     // cusparse
-    cusparseErrCheck(cusparseCreateCsr(&d_matDescr_sparse, h_rows, h_columns, h_nnz, d_csr_offsets, d_csr_columns, d_csr_values, CUSPARSE_INDEX_32I,
+    cusparseErrCheck(cusparseCreateCsr(&spMatDescr, h_rows, h_columns, h_nnz, d_csrRowPtr, d_csrColInd, d_csrVal, CUSPARSE_INDEX_32I,
                                        CUSPARSE_INDEX_32I, CUSPARSE_INDEX_BASE_ZERO, CUDA_R_64F));
   }
   MatrixCUSPARSE(unsigned r, unsigned c, double *m) : h_rows(r), h_columns(c) { // Constructor (entry point)
     printf("MatrixCUSPARSE entry point called\n");
     // convert dense to sparse
-    cusparseDnMatDescr_t d_matDescr_dense;
+    cusparseDnMatDescr_t dnMatDescr;
     double *d_mat;
     cudaErrCheck(cudaMalloc((void **)&d_rows, sizeof(unsigned)));
     cudaErrCheck(cudaMalloc((void **)&d_columns, sizeof(unsigned)));
@@ -68,28 +68,27 @@ public:
     cusparseErrCheck(cusparseSetMatType(descr, CUSPARSE_MATRIX_TYPE_GENERAL));
     cusparseErrCheck(cusparseSetMatIndexBase(descr, CUSPARSE_INDEX_BASE_ZERO));
     // csr_offset ptr
-    cudaErrCheck(cudaMalloc((void **)&d_csr_offsets, (r + 1) * sizeof(int)));
+    cudaErrCheck(cudaMalloc((void **)&d_csrRowPtr, (r + 1) * sizeof(int)));
     // cusparse dense and sparse matricies
-    cusparseErrCheck(cusparseCreateDnMat(&d_matDescr_dense, r, c, c, d_mat, CUDA_R_64F, CUSPARSE_ORDER_ROW));
-    cusparseErrCheck(cusparseCreateCsr(&d_matDescr_sparse, r, c, 0, d_csr_offsets, NULL, NULL, CUSPARSE_INDEX_32I, CUSPARSE_INDEX_32I,
+    cusparseErrCheck(cusparseCreateDnMat(&dnMatDescr, r, c, c, d_mat, CUDA_R_64F, CUSPARSE_ORDER_ROW));
+    cusparseErrCheck(cusparseCreateCsr(&spMatDescr, r, c, 0, d_csrRowPtr, NULL, NULL, CUSPARSE_INDEX_32I, CUSPARSE_INDEX_32I,
                                        CUSPARSE_INDEX_BASE_ZERO, CUDA_R_64F));
-    cusparseErrCheck(
-        cusparseDenseToSparse_bufferSize(spHandle, d_matDescr_dense, d_matDescr_sparse, CUSPARSE_DENSETOSPARSE_ALG_DEFAULT, &bufferSize));
+    cusparseErrCheck(cusparseDenseToSparse_bufferSize(spHandle, dnMatDescr, spMatDescr, CUSPARSE_DENSETOSPARSE_ALG_DEFAULT, &bufferSize));
     cudaErrCheck(cudaMalloc(&dBuffer, bufferSize));
-    cusparseErrCheck(cusparseDenseToSparse_analysis(spHandle, d_matDescr_dense, d_matDescr_sparse, CUSPARSE_DENSETOSPARSE_ALG_DEFAULT, dBuffer));
+    cusparseErrCheck(cusparseDenseToSparse_analysis(spHandle, dnMatDescr, spMatDescr, CUSPARSE_DENSETOSPARSE_ALG_DEFAULT, dBuffer));
 
     int64_t rows_tmp, cols_tmp;
-    cusparseErrCheck(cusparseSpMatGetSize(d_matDescr_sparse, &rows_tmp, &cols_tmp, &h_nnz)); // determine # of non-zeros
+    cusparseErrCheck(cusparseSpMatGetSize(spMatDescr, &rows_tmp, &cols_tmp, &h_nnz)); // determine # of non-zeros
     // printf("Sparse matrix created, rows=%d, cols=%d, nnz=%d\n", rows_tmp, cols_tmp, h_nnz);
-    cudaErrCheck(cudaMalloc((void **)&d_csr_columns, h_nnz * sizeof(int)));
-    cudaErrCheck(cudaMalloc((void **)&d_csr_values, h_nnz * sizeof(double)));
+    cudaErrCheck(cudaMalloc((void **)&d_csrColInd, h_nnz * sizeof(int)));
+    cudaErrCheck(cudaMalloc((void **)&d_csrVal, h_nnz * sizeof(double)));
 
-    cusparseErrCheck(cusparseCsrSetPointers(d_matDescr_sparse, d_csr_offsets, d_csr_columns, d_csr_values));
-    cusparseErrCheck(cusparseDenseToSparse_convert(spHandle, d_matDescr_dense, d_matDescr_sparse, CUSPARSE_DENSETOSPARSE_ALG_DEFAULT, dBuffer));
-    cusparseDestroyDnMat(d_matDescr_dense);
+    cusparseErrCheck(cusparseCsrSetPointers(spMatDescr, d_csrRowPtr, d_csrColInd, d_csrVal));
+    cusparseErrCheck(cusparseDenseToSparse_convert(spHandle, dnMatDescr, spMatDescr, CUSPARSE_DENSETOSPARSE_ALG_DEFAULT, dBuffer));
+    cusparseDestroyDnMat(dnMatDescr);
     cudaFree(d_mat);
   };
-  MatrixCUSPARSE(const MatrixCUSPARSE &m) : MatrixCUSPARSE(m.h_rows, m.h_columns, m.h_nnz, m.d_csr_values, m.d_csr_columns, m.d_csr_offsets) {
+  MatrixCUSPARSE(const MatrixCUSPARSE &m) : MatrixCUSPARSE(m.h_rows, m.h_columns, m.h_nnz, m.d_csrVal, m.d_csrColInd, m.d_csrRowPtr) {
     printf("MatrixCUSPARSE Copy Constructor\n");
   };                                                   // Copy constructor
   MatrixCUSPARSE &operator=(const MatrixCUSPARSE &m) { // Copy assignment operator
@@ -98,78 +97,78 @@ public:
     h_columns = m.h_columns;
     h_nnz = m.h_nnz;
     // destroy old allocation
-    cudaErrCheck(cudaFree(d_csr_values));
-    cudaErrCheck(cudaFree(d_csr_columns));
-    cudaErrCheck(cudaFree(d_csr_offsets));
-    cusparseErrCheck(cusparseDestroySpMat(d_matDescr_sparse));
+    cudaErrCheck(cudaFree(d_csrVal));
+    cudaErrCheck(cudaFree(d_csrColInd));
+    cudaErrCheck(cudaFree(d_csrRowPtr));
+    cusparseErrCheck(cusparseDestroySpMat(spMatDescr));
     // memory allocation
-    cudaErrCheck(cudaMalloc((void **)&d_csr_values, sizeof(double) * h_nnz));
-    cudaErrCheck(cudaMalloc((void **)&d_csr_columns, sizeof(int) * h_nnz));
-    cudaErrCheck(cudaMalloc((void **)&d_csr_offsets, sizeof(int) * (h_rows + 1)));
+    cudaErrCheck(cudaMalloc((void **)&d_csrVal, sizeof(double) * h_nnz));
+    cudaErrCheck(cudaMalloc((void **)&d_csrColInd, sizeof(int) * h_nnz));
+    cudaErrCheck(cudaMalloc((void **)&d_csrRowPtr, sizeof(int) * (h_rows + 1)));
     // copy to device
     cudaErrCheck(cudaMemcpy(d_rows, m.d_rows, sizeof(unsigned), cudaMemcpyDeviceToDevice));
     cudaErrCheck(cudaMemcpy(d_columns, m.d_columns, sizeof(unsigned), cudaMemcpyDeviceToDevice));
-    cudaErrCheck(cudaMemcpy(d_csr_values, m.d_csr_values, h_nnz * sizeof(double), cudaMemcpyDeviceToDevice));
-    cudaErrCheck(cudaMemcpy(d_csr_columns, m.d_csr_columns, h_nnz * sizeof(int), cudaMemcpyDeviceToDevice));
-    cudaErrCheck(cudaMemcpy(d_csr_offsets, m.d_csr_offsets, (m.h_rows + 1) * sizeof(int), cudaMemcpyDeviceToDevice));
+    cudaErrCheck(cudaMemcpy(d_csrVal, m.d_csrVal, h_nnz * sizeof(double), cudaMemcpyDeviceToDevice));
+    cudaErrCheck(cudaMemcpy(d_csrColInd, m.d_csrColInd, h_nnz * sizeof(int), cudaMemcpyDeviceToDevice));
+    cudaErrCheck(cudaMemcpy(d_csrRowPtr, m.d_csrRowPtr, (m.h_rows + 1) * sizeof(int), cudaMemcpyDeviceToDevice));
     // cusparse
-    cusparseErrCheck(cusparseCreateCsr(&d_matDescr_sparse, h_rows, h_columns, h_nnz, d_csr_offsets, d_csr_columns, d_csr_values, CUSPARSE_INDEX_32I,
+    cusparseErrCheck(cusparseCreateCsr(&spMatDescr, h_rows, h_columns, h_nnz, d_csrRowPtr, d_csrColInd, d_csrVal, CUSPARSE_INDEX_32I,
                                        CUSPARSE_INDEX_32I, CUSPARSE_INDEX_BASE_ZERO, CUDA_R_64F));
     return *this;
   };
-  MatrixCUSPARSE(MatrixCUSPARSE &&m) noexcept : MatrixCUSPARSE(m.h_rows, m.h_columns, m.h_nnz, m.d_csr_values, m.d_csr_columns, m.d_csr_offsets) {
+  MatrixCUSPARSE(MatrixCUSPARSE &&m) noexcept : MatrixCUSPARSE(m.h_rows, m.h_columns, m.h_nnz, m.d_csrVal, m.d_csrColInd, m.d_csrRowPtr) {
     printf("MatrixCUSPARSE Move Constructor called\n");
     // free old resources
-    cudaErrCheck(cudaFree(m.d_csr_values));
-    cudaErrCheck(cudaFree(m.d_csr_offsets));
-    cudaErrCheck(cudaFree(m.d_csr_columns));
-    cusparseErrCheck(cusparseDestroySpMat(m.d_matDescr_sparse));
+    cudaErrCheck(cudaFree(m.d_csrVal));
+    cudaErrCheck(cudaFree(m.d_csrRowPtr));
+    cudaErrCheck(cudaFree(m.d_csrColInd));
+    cusparseErrCheck(cusparseDestroySpMat(m.spMatDescr));
     m.h_rows = ZERO;
     m.h_nnz = ZERO;
     m.h_columns = ZERO;
-    cudaErrCheck(cudaMalloc((void **)&m.d_csr_values, h_nnz * sizeof(double)));
-    cudaErrCheck(cudaMalloc((void **)&m.d_csr_columns, h_nnz * sizeof(int)));
-    cudaErrCheck(cudaMalloc((void **)&m.d_csr_offsets, (h_rows + 1) * sizeof(int)));
+    cudaErrCheck(cudaMalloc((void **)&m.d_csrVal, h_nnz * sizeof(double)));
+    cudaErrCheck(cudaMalloc((void **)&m.d_csrColInd, h_nnz * sizeof(int)));
+    cudaErrCheck(cudaMalloc((void **)&m.d_csrRowPtr, (h_rows + 1) * sizeof(int)));
     cudaErrCheck(cudaMemcpy(m.d_rows, &m.h_rows, sizeof(unsigned), cudaMemcpyHostToDevice));
     cudaErrCheck(cudaMemcpy(m.d_columns, &m.h_columns, sizeof(unsigned), cudaMemcpyHostToDevice));
-    cudaErrCheck(cudaMemset(m.d_csr_values, ZERO, m.h_nnz * sizeof(double)));
-    cudaErrCheck(cudaMemset(m.d_csr_columns, ZERO, m.h_nnz * sizeof(int)));
-    cudaErrCheck(cudaMemset(m.d_csr_offsets, ZERO, (m.h_rows + 1) * sizeof(int)));
-    cusparseErrCheck(cusparseCreateCsr(&m.d_matDescr_sparse, m.h_rows, m.h_columns, m.h_nnz, m.d_csr_offsets, m.d_csr_columns, m.d_csr_values,
-                                       CUSPARSE_INDEX_32I, CUSPARSE_INDEX_32I, CUSPARSE_INDEX_BASE_ZERO, CUDA_R_64F));
+    cudaErrCheck(cudaMemset(m.d_csrVal, ZERO, m.h_nnz * sizeof(double)));
+    cudaErrCheck(cudaMemset(m.d_csrColInd, ZERO, m.h_nnz * sizeof(int)));
+    cudaErrCheck(cudaMemset(m.d_csrRowPtr, ZERO, (m.h_rows + 1) * sizeof(int)));
+    cusparseErrCheck(cusparseCreateCsr(&m.spMatDescr, m.h_rows, m.h_columns, m.h_nnz, m.d_csrRowPtr, m.d_csrColInd, m.d_csrVal, CUSPARSE_INDEX_32I,
+                                       CUSPARSE_INDEX_32I, CUSPARSE_INDEX_BASE_ZERO, CUDA_R_64F));
   };
   MatrixCUSPARSE &operator=(MatrixCUSPARSE &&m) noexcept { // Move assignment operator
     printf("MatrixCUSPARSE Copy Assignment called\n");
     // call copy assignment
     *this = m;
     // free old resources
-    cudaErrCheck(cudaFree(m.d_csr_values));
-    cudaErrCheck(cudaFree(m.d_csr_offsets));
-    cudaErrCheck(cudaFree(m.d_csr_columns));
-    cusparseErrCheck(cusparseDestroySpMat(m.d_matDescr_sparse));
+    cudaErrCheck(cudaFree(m.d_csrVal));
+    cudaErrCheck(cudaFree(m.d_csrRowPtr));
+    cudaErrCheck(cudaFree(m.d_csrColInd));
+    cusparseErrCheck(cusparseDestroySpMat(m.spMatDescr));
     m.h_rows = ZERO;
     m.h_nnz = ZERO;
     m.h_columns = ZERO;
-    cudaErrCheck(cudaMalloc((void **)&m.d_csr_values, h_nnz * sizeof(double)));
-    cudaErrCheck(cudaMalloc((void **)&m.d_csr_columns, h_nnz * sizeof(int)));
-    cudaErrCheck(cudaMalloc((void **)&m.d_csr_offsets, (h_rows + 1) * sizeof(int)));
+    cudaErrCheck(cudaMalloc((void **)&m.d_csrVal, h_nnz * sizeof(double)));
+    cudaErrCheck(cudaMalloc((void **)&m.d_csrColInd, h_nnz * sizeof(int)));
+    cudaErrCheck(cudaMalloc((void **)&m.d_csrRowPtr, (h_rows + 1) * sizeof(int)));
     cudaErrCheck(cudaMemcpy(m.d_rows, &m.h_rows, sizeof(unsigned), cudaMemcpyHostToDevice));
     cudaErrCheck(cudaMemcpy(m.d_columns, &m.h_columns, sizeof(unsigned), cudaMemcpyHostToDevice));
-    cudaErrCheck(cudaMemset(m.d_csr_values, ZERO, m.h_nnz * sizeof(double)));
-    cudaErrCheck(cudaMemset(m.d_csr_columns, ZERO, m.h_nnz * sizeof(int)));
-    cudaErrCheck(cudaMemset(m.d_csr_offsets, ZERO, (m.h_rows + 1) * sizeof(int)));
-    cusparseErrCheck(cusparseCreateCsr(&m.d_matDescr_sparse, m.h_rows, m.h_columns, m.h_nnz, m.d_csr_offsets, m.d_csr_columns, m.d_csr_values,
-                                       CUSPARSE_INDEX_32I, CUSPARSE_INDEX_32I, CUSPARSE_INDEX_BASE_ZERO, CUDA_R_64F));
+    cudaErrCheck(cudaMemset(m.d_csrVal, ZERO, m.h_nnz * sizeof(double)));
+    cudaErrCheck(cudaMemset(m.d_csrColInd, ZERO, m.h_nnz * sizeof(int)));
+    cudaErrCheck(cudaMemset(m.d_csrRowPtr, ZERO, (m.h_rows + 1) * sizeof(int)));
+    cusparseErrCheck(cusparseCreateCsr(&m.spMatDescr, m.h_rows, m.h_columns, m.h_nnz, m.d_csrRowPtr, m.d_csrColInd, m.d_csrVal, CUSPARSE_INDEX_32I,
+                                       CUSPARSE_INDEX_32I, CUSPARSE_INDEX_BASE_ZERO, CUDA_R_64F));
     return *this;
   };
   ~MatrixCUSPARSE() {
-    cusparseErrCheck(cusparseDestroySpMat(d_matDescr_sparse));
+    cusparseErrCheck(cusparseDestroySpMat(spMatDescr));
     cusparseErrCheck(cusparseDestroyMatDescr(descr));
     cudaErrCheck(cudaFree(d_rows));
     cudaErrCheck(cudaFree(d_columns));
-    cudaErrCheck(cudaFree(d_csr_offsets));
-    cudaErrCheck(cudaFree(d_csr_columns));
-    cudaErrCheck(cudaFree(d_csr_values));
+    cudaErrCheck(cudaFree(d_csrRowPtr));
+    cudaErrCheck(cudaFree(d_csrColInd));
+    cudaErrCheck(cudaFree(d_csrVal));
     cudaErrCheck(cudaFree(dBuffer));
   };
   Vector_GPU operator*(Vector_GPU &v); // Multiplication
